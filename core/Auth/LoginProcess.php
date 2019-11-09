afaf63c55da38db860a7bbd0fee050052d3c1ce1
Anton Hideger <antonhideger1337@gmail.com>
2019-11-09 14:12:07 +0300

    have create a remember me button and a method which check cookie avalability

--- core/Auth/LoginProcess.php
+++ core/Auth/LoginProcess.php
@@ -3,11 +3,49 @@
 namespace Gomail\Auth;
 
 use Application\Models\User;
+use Application\Controllers\Auth\RememberMeController;
 
 class LoginProcess extends Authentificate
 {
+    /**
+     * @var \Gomail\Request\Session
+     */ 
     protected $message;
 
+    /**
+     * @var \Application\Controllers\Auth\RememberMeController
+     */ 
+    protected $remember;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->remember = new RememberMeController();
+    }
+
+    /**
+     * Remember user for 10 days
+     * 
+     * @param $input string
+     * 
+     * @return \Gomail\Request\Cookie
+     */ 
+    public function remember($input)
+    {
+        return $this->remember->rememberMe($input);
+    }
+
+    /**
+     * Login user. Validate fields and return success or error
+     * message. If error message doesnt exists call loginUser
+     * method of User model and finally check if was clicked rememeber 
+     * me button and create a login cookie
+     * 
+     * @param $fields array
+     * 
+     * @return \Gomail\Request\Cookie
+     */ 
     public function login($fields)
     {
         if (!is_array($fields)) {
@@ -20,8 +58,14 @@ class LoginProcess extends Authentificate
             return false;
         }
 
-        if ($this->user->loginUser($fields)) {
-            return $this->cookie('login', "{$fields['email']}", time() + 86400 * 5)->set();
+        if (!$this->user->loginUser($fields)) {
+            return $this->session('error', 'Неправильная почта или пароль');
+        }   
+
+        if (!$this->remember($fields['check'])) {
+            return $this->cookie('login', $this->post()['email'], time() + 86400)->set();
         }
+
+        return $this->remember($fields['check']);
     }
 }
